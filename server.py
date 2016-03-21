773db43af5b2dba0fb678b30267c0d26e311d5cf
Richard Chen <richardchen39@gmail.com>
2016-03-21 15:01:51 -0700

    link backend to frontend

--- server.py
+++ server.py
@@ -4,10 +4,22 @@ from werkzeug import secure_filename
 from subprocess import call
 import lib.Python.master as master
 
-
 app = Flask(__name__, static_url_path='')
 
+# example parameters
+FRAME_FOLDER = "lib/downloads/frames"
+VIDEO_LOCATION = "lib/downloads/video.mp4"
+SLIDE_LOCATION = "lib/downloads/slide.pdf"
+SLIDE_FOLDER = "lib/downloads/slides"
 
+'''
+returns JSON string with an array of JSON objects. Each object has:
+	image: filename of the slide inside of SLIDE_FOLDER
+	timestamp: time in seconds corresponding to the slide
+locations should be relative to the Slip folder, as this method automatically moves to that folder
+'''
+def getTimeStamps(slideLocation, videoLocation, slideFolder, frameFolder):
+	return master.findFrames(slideLocation, videoLocation, slideFolder, frameFolder)
 
 def download_files(video_link, slide_link):
 	call(['curl', '-o', VIDEO_LOCATION, video_link])
@@ -23,22 +35,8 @@ def get_links():
 	slide_link = request.form['slide']
 	if video_link.endswith('.mp4') and slide_link.endswith('.pdf'):
 		download_files(video_link, slide_link)
+		return getTimeStamps(SLIDE_LOCATION, VIDEO_LOCATION, SLIDE_LOCATION, FRAME_FOLDER)
 	return redirect('/')
-
-#example parameters
-FRAME_FOLDER = "lib/downloads/frames"
-VIDEO_LOCATION = "lib/downloads/video.mp4"
-SLIDE_LOCATION = "lib/downloads/slide.pdf"
-SLIDE_FOLDER = "lib/downloads/slides"
-
-'''
-returns JSON string with an array of JSON objects. Each object has:
-	image: filename of the slide inside of SLIDE_FOLDER
-	timestamp: time in seconds corresponding to the slide
-locations should be relative to the Slip folder, as this method automatically moves to that folder
-'''
-def getTimeStampes(slideLocation, videoLocation, slideFolder, frameFolder):
- return master.findFrames(slideLocation, videoLocation, slideFolder, frameFolder)
 
 if __name__ == "__main__":
 	app.run()
