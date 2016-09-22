4556e08c613b3358e26b294dfd4e90cf85136e43
Richard Chen <richardchen39@gmail.com>
2016-09-22 11:39:25 -0500

    Add new routes

--- app/routes/index.py
+++ app/routes/index.py
@@ -3,13 +3,12 @@ from app import app
 from werkzeug.utils import secure_filename
 import os
 
-UPLOAD_FOLDER = 'files/before'
-ALLOWED_EXTENSIONS = set(['pdf', 'mp4'])
-
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['BEFORE_FOLDER'] = 'files/before'
+app.config['AFTER_FOLDER'] = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(
+    os.path.abspath(__file__)))), 'files/after')
 
 def allowed_file(filename):
-  return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+  return '.' in filename and filename.rsplit('.', 1)[1] in set(['pdf', 'mp4'])
 
 def save_file(file_part):
   if file_part not in request.files:
@@ -23,16 +22,25 @@ def save_file(file_part):
 
   if file and allowed_file(file.filename):
     filename = secure_filename(file.filename)
-    file.save(os.path.join(app.config['UPLOAD_FOLDER'] + '/' + file_part, filename))
+    file.save(os.path.join(app.config['BEFORE_FOLDER'] + '/' + file_part, filename))
 
-@app.route('/upload', methods=['GET', 'POST'])
+@app.route('/upload', methods=['POST'])
 def upload_file():
-  if request.method == 'POST':
-    save_file('video')
-    save_file('slides')    
-    return render_template('index.html')
-  else:
-    return render_template('index.html')
+  save_file('video')
+  save_file('slides')    
+  return render_template('player.html')
+
+@app.route('/slide', methods=['GET'])
+def get_slide():
+  return send_from_directory(app.config['AFTER_FOLDER'] + '/slides', request.args.get('filename'))
+
+@app.route('/video', methods=['GET'])
+def get_video():
+  return send_from_directory(app.config['BEFORE_FOLDER'] + '/video', 'video.mp4') # TODO: filename constant
+
+@app.route('/player', methods=['GET'])
+def player():
+  return render_template('player.html')
 
 @app.route('/', methods=['GET'])
 def index():
